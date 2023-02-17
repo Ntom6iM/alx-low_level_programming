c4dddb515c0fec1743127d349886604f1d8e3407
Ntom6iM <ntombenhlemabuza68@gmail.com>
2023-02-16 18:05:34 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	char s[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fprintf(stderr, "%s", s);
 		return (1);
 }
